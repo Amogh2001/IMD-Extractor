bd1d6b191d031e6c123aaf93209cb7e75694e595
Amogh Sanjay <amoghamogh2001@gmail.com>
2023-07-31 12:14:14 +0530

    Update imd_1x1_prac.py

--- imd_1x1_prac.py
+++ imd_1x1_prac.py
@@ -31,124 +31,166 @@ for inc_imd in list_year:
         imd_arr = np.append(imd_arr, imd_year)
 #print(imd_arr[3630:])
 
-#--------------------------------------GKVK Data-----------------------------------------
+#--------------------------------------Station 1 Data-----------------------------------------
 
 list01_11 = ['AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AN', 'AO', 'AP', 'AQ'] #include 'AM' for 2007
 list12_22 = ['AR', 'AS','AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ']
 list2 =['D','H', 'L', 'P', 'T','X', 'AB', 'AF', 'AJ', 'AN', 'AR']
 
-bang_gkvk_df4 = pd.DataFrame()
-bang_gkvk_df5 = pd.DataFrame()
+
+bang_stat1_df4 = pd.DataFrame()
+bang_stat1_df5 = pd.DataFrame()
 
 
 for m in list01_11:
-    bang_gkvk4 = pd.read_excel('GKVK_DailyRainfall_1.1972-2022.xlsx', usecols=m)
-    bang_gkvk_df4 = pd.concat([bang_gkvk_df4, (bang_gkvk4[1:369])], axis = 0, ignore_index= True)
-bang_gkvk_df4 = bang_gkvk_df4.stack().reset_index()
-#print(bang_gkvk_df3.loc[110:130])
+    bang_stat1_4 = pd.read_excel('DataSheet1.xlsx', usecols=m)
+    bang_stat1_df4 = pd.concat([bang_stat1_df4, (bang_stat1_4[1:369])], axis = 0, ignore_index= True)
+bang_stat1_df4 = bang_stat1_df4.stack().reset_index()
+
 
 for n in list12_22:
-    bang_gkvk5 = pd.read_excel('GKVK_DailyRainfall_1.1972-2022.xlsx', usecols=n)
-    bang_gkvk_df5 = pd.concat([bang_gkvk_df5, (bang_gkvk5[1:369])], axis = 0, ignore_index= True)
-bang_gkvk_df5 = bang_gkvk_df5.stack().reset_index()
+    bang_stat1_5 = pd.read_excel('DataSheet1.xlsx', usecols=n)
+    bang_stat1_df5 = pd.concat([bang_stat1_df5, (bang_stat1_5[1:369])], axis = 0, ignore_index= True)
+bang_stat1_df5 = bang_stat1_df5.stack().reset_index()
 
-bang_gkvk_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
-bang_gkvk_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat1_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat1_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
 
-bang_gkvk_df4['Rainfall'] = pd.to_numeric(bang_gkvk_df4['Rainfall'], errors = 'coerce')
-df4_rarr = bang_gkvk_df4['Rainfall'].array
+
+bang_stat1_df4['Rainfall'] = pd.to_numeric(bang_stat1_df4['Rainfall'], errors = 'coerce')
+df4_rarr = bang_stat1_df4['Rainfall'].array
 df4_rarr = np.insert(df4_rarr, 0, 0.0)
 #df4_rarr = np.delete(df4_rarr, 4017)   #include when including 2007
 
-bang_gkvk_df5['Rainfall'] = pd.to_numeric(bang_gkvk_df5['Rainfall'], errors = 'coerce')
-df5_rarr = bang_gkvk_df5['Rainfall'].array
+bang_stat1_df5['Rainfall'] = pd.to_numeric(bang_stat1_df5['Rainfall'], errors = 'coerce')
+df5_rarr = bang_stat1_df5['Rainfall'].array
 df5_rarr = np.insert(df5_rarr, 0, 0.0)
-#df5_rarr = np.delete(df5_rarr, 3288)
-#print(df1_rarr[730:740])
+df5_rarr = np.delete(df5_rarr, 3288)
 
-#--------------------------------------HAL Airport Data-----------------------------------------
+bang_stat1_4_month2001 = dc.month_rain_getter(df4_rarr[1:369])
+bang_stat1_4_array = np.array(bang_stat1_4_month2001)
+bang_stat1_4_array_test = np.array(bang_stat1_4_month2001)
 
-list01_11 = [ 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AQ', 'AR', 'AS','AT']  #2007 is 'AP'
+for num1 in range(1, 11):
+    num2 = num1 + 1
+    if num2/4 ==1 or num2/7 == 1:
+        bang_stat1_4_iter = dc.month_rain_getter(df4_rarr[(369*num1):(369*num2)], leap = True)
+        bang_stat1_4_array_test = np.append(bang_stat1_4_array_test, bang_stat1_4_iter)
+    else:
+        bang_stat1_4_iter = dc.month_rain_getter(df4_rarr[(369*num1):(369*num2)])
+        bang_stat1_4_array_test = np.append(bang_stat1_4_array_test, bang_stat1_4_iter)
+
+
+
+#========================================== Station 2 Data ============================================
+
+list01_11 = ['AJ', 'AK', 'AL', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS','AT']
 list12_22 = ['AU', 'AV']#, 'AW', 'AX', 'AY', 'AZ']
 list2 =['D','H', 'L', 'P', 'T','X', 'AB', 'AF', 'AJ', 'AN', 'AR']
 
-bang_hal_df4 = pd.DataFrame()
-bang_hal_df5 = pd.DataFrame()
+bang_stat2_df4 = pd.DataFrame()
+bang_stat2_df5 = pd.DataFrame()
 
 for m in list01_11:
-    bang_hal4 = pd.read_excel('HAL_Revised_1969.xlsx', usecols=m)
-    bang_hal_df4 = pd.concat([bang_hal_df4, (bang_hal4[1:369])], axis = 0, ignore_index= True)
-bang_hal_df4 = bang_hal_df4.stack().reset_index()
-#print(bang_hal_df3.loc[110:130])
+    bang_stat2_4 = pd.read_excel('DataSheet_2.xlsx', usecols=m)
+    bang_stat2_df4 = pd.concat([bang_stat2_df4, (bang_stat2_4[1:368])], axis = 0, ignore_index= True)
+bang_stat2_df4 = bang_stat2_df4.stack().reset_index()
 
 for n in list12_22:
-    bang_hal5 = pd.read_excel('HAL_Revised_1969.xlsx', usecols=n)
-    bang_hal_df5 = pd.concat([bang_hal_df5, (bang_hal5[1:368])], axis = 0, ignore_index= True)
-bang_hal_df5 = bang_hal_df5.stack().reset_index()
+    bang_stat2_5 = pd.read_excel('DataSheet_2.xlsx', usecols=n)
+    bang_stat2_df5 = pd.concat([bang_stat2_df5, (bang_stat2_5[1:368])], axis = 0, ignore_index= True)
+bang_stat2_df5 = bang_stat2_df5.stack().reset_index()
 
-bang_hal_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
-bang_hal_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat2_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat2_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
+
+bang_stat2_df4['Rainfall'] = pd.to_numeric(bang_stat2_df4['Rainfall'], errors = 'coerce')
+df4_rarr_stat2 = bang_stat2_df4['Rainfall'].array
+df4_rarr_stat2 = np.insert(df4_rarr_stat2, 0, 0.0)
+#df4_rarr_stat2 = np.delete(df4_rarr_stat2, 4017)
+
+bang_stat2_df5['Rainfall'] = pd.to_numeric(bang_stat2_df5['Rainfall'], errors = 'coerce')
+df5_rarr_stat2 = bang_stat2_df5['Rainfall'].array
+df5_rarr_stat2 = np.insert(df5_rarr_stat2, 0, 0.0)
+#df5_rarr_stat2 = np.delete(df5_rarr_stat2, 3288)
+
+bang_stat2_4_month2001 = dc.month_rain_getter(df4_rarr_stat2[1:368])
+bang_stat2_4_array = np.array(bang_stat2_4_month2001)
+bang_stat2_4_array_test = np.array(bang_stat2_4_month2001)
+
+for num1_stat2 in range(1, 11):
+    num2_stat2 = num1_stat2 + 1
+    if num2_stat2 % 4 == 0:
+        bang_stat2_4_iter = dc.month_rain_getter(df4_rarr_stat2[(368*num1_stat2):(368*num2_stat2)], leap = True)
+        bang_stat2_4_array_test = np.append(bang_stat2_4_array_test, bang_stat2_4_iter)
+    else:
+        bang_stat2_4_iter = dc.month_rain_getter(df4_rarr_stat2[(368*num1_stat2):(368*num2_stat2)])
+        bang_stat2_4_array_test = np.append(bang_stat2_4_array_test, bang_stat2_4_iter)
 
 
-bang_hal_df4['Rainfall'] = pd.to_numeric(bang_hal_df4['Rainfall'], errors = 'coerce')
-df4_rarr_hal = bang_hal_df4['Rainfall'].array
-df4_rarr_hal = np.insert(df4_rarr_hal, 0, 0.0)
-#df4_rarr_hal = np.delete(df4_rarr_hal, 4017)
+#==================================== Station 3 Data=============================
 
-bang_hal_df5['Rainfall'] = pd.to_numeric(bang_hal_df5['Rainfall'], errors = 'coerce')
-df5_rarr_hal = bang_hal_df5['Rainfall'].array
-df5_rarr_hal = np.insert(df5_rarr_hal, 0, 0.0)
-#df5_rarr_hal = np.delete(df5_rarr_hal, 3288)
-
-#--------------------------------------City Central College Data-----------------------------------------
-
-list01_11 = [ 'AJ', 'AK', 'AL','AM', 'AN', 'AO', 'AQ', 'AR', 'AS','AT']
+list01_11 = [ 'AJ', 'AK', 'AL', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS','AT']
 list12_22 = ['AU', 'AV']#, 'AW', 'AX', 'AY', 'AZ']
 list2 =['D','H', 'L', 'P', 'T','X', 'AB', 'AF', 'AJ', 'AN', 'AR']
 
-bang_ccc_df4 = pd.DataFrame()
-bang_ccc_df5 = pd.DataFrame()
-
+bang_stat3_df4 = pd.DataFrame()
+bang_stat3_df5 = pd.DataFrame()
 
 for m in list01_11:
-    bang_ccc4 = pd.read_excel('/media/amogh01/One Touch/prac/City_Central_College_Revised_69-14.xlsx', usecols=m)
-    bang_ccc_df4 = pd.concat([bang_ccc_df4, (bang_ccc4[1:368])], axis = 0, ignore_index= True)
-bang_ccc_df4 = bang_ccc_df4.stack().reset_index()
-#print(bang_ccc_df4[340:370])
+    bang_stat3_4 = pd.read_excel('DataSheet_3.xlsx', usecols=m)
+    bang_stat3_df4 = pd.concat([bang_stat3_df4, (bang_stat3_4[1:368])], axis = 0, ignore_index= True)
+bang_stat3_df4 = bang_stat3_df4.stack().reset_index()
+
 
 for n in list12_22:
-    bang_ccc5 = pd.read_excel('/media/amogh01/One Touch/prac/City_Central_College_Revised_69-14.xlsx', usecols=n)
-    bang_ccc_df5 = pd.concat([bang_ccc_df5, (bang_ccc5[1:368])], axis = 0, ignore_index= True)
-bang_ccc_df5 = bang_ccc_df5.stack().reset_index()
+    bang_stat3_5 = pd.read_excel('DataSheet_3.xlsx', usecols=n)
+    bang_stat3_df5 = pd.concat([bang_stat3_df5, (bang_stat3_5[1:369])], axis = 0, ignore_index= True)
+bang_stat3_df5 = bang_stat3_df5.stack().reset_index()
 
-bang_ccc_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
-bang_ccc_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat3_df4.rename(columns = {0: 'Rainfall'}, inplace=True)
+bang_stat3_df5.rename(columns = {0: 'Rainfall'}, inplace=True)
+
+bang_stat3_df4['Rainfall'] = pd.to_numeric(bang_stat3_df4['Rainfall'], errors = 'coerce')
+df4_rarr_stat3 = bang_stat3_df4['Rainfall'].array
+df4_rarr_stat3 = np.insert(df4_rarr_stat3, 0, 0.0)
+#df4_rarr = np.delete(df4_rarr, 4017)
+
+bang_stat3_df5['Rainfall'] = pd.to_numeric(bang_stat3_df5['Rainfall'], errors = 'coerce')
+df5_rarr_stat3 = bang_stat3_df5['Rainfall'].array
+df5_rarr_stat3 = np.insert(df5_rarr_stat3, 0, 0.0)
+#df5_rarr_stat3 = np.delete(df5_rarr_stat3, 3288)
 
 
-bang_ccc_df4['Rainfall'] = pd.to_numeric(bang_ccc_df4['Rainfall'], errors = 'coerce')
-df4_rarr_ccc = bang_ccc_df4['Rainfall'].array
-df4_rarr_ccc = np.insert(df4_rarr_ccc, 0, 0.0)
-#df4_rarr_ccc = np.delete(df4_rarr_ccc, 4017)   #include when including 2007
+bang_stat3_4_month2001 = dc.month_rain_getter(df4_rarr_stat3[1:368])
+bang_stat3_4_array = np.array(bang_stat3_4_month2001)
+bang_stat3_4_array_test = np.array(bang_stat3_4_month2001)
 
-bang_ccc_df5['Rainfall'] = pd.to_numeric(bang_ccc_df5['Rainfall'], errors = 'coerce')
-df5_rarr_ccc = bang_ccc_df5['Rainfall'].array
-df5_rarr_ccc = np.insert(df5_rarr_ccc, 0, 0.0)
-#df5_rarr_ccc = np.delete(df5_rarr_ccc, 3288)
+for num1_stat3 in range(1, 11):
+    num2_stat3 = num1_stat3 + 1
+    if num2_stat3 % 4 == 0:
+        bang_stat3_4_iter = dc.month_rain_getter(df4_rarr_stat3[(368*num1_stat3):(368*num2_stat3)], leap = True)
+        bang_stat3_4_array_test = np.append(bang_stat3_4_array_test, bang_stat3_4_iter)
+    else:
+        bang_stat3_4_iter = dc.month_rain_getter(df4_rarr_stat3[(368*num1_stat3):(368*num2_stat3)])
+        bang_stat3_4_array_test = np.append(bang_stat3_4_array_test, bang_stat3_4_iter)
 
-#===============================================  Finding Mean of 3 Stations ====================================
+#================================ Mean of 3 stations =======================================
 
-df4_gkvk = pd.DataFrame(df4_rarr)
-df4_hal = pd.DataFrame(df4_rarr_hal)
-df4_ccc = pd.DataFrame(df4_rarr_ccc)
 
-comb_1 = [df4_gkvk, df4_hal]
-df_com1 = pd.concat(comb_1, axis = 1, join = 'inner')
+stat1_df1 = pd.DataFrame(bang_stat1_4_array_test)
+stat2_df1 = pd.DataFrame(bang_stat2_4_array_test)
+stat3_df1 = pd.DataFrame(bang_stat3_4_array_test)
 
-comb_2 = [df_com1, df4_ccc]
-df_com2 = pd.concat(comb_2, axis = 1, join = 'inner')
 
-df_com_sum = df_com2.sum(axis = 1)
-df_com_mean = df_com_sum.div(3.0)
+frames_1_mon = [stat1_df1, stat3_df1]
+mon_cat_1 = pd.concat(frames_1_mon, axis = 1, join = 'inner')
+
+frames_2_mon = [mon_cat_1, stat2_df1]
+mon_cat2 = pd.concat(frames_2_mon, axis = 1, join = 'inner')
+
+mon_sum1 = mon_cat2.sum(axis=1)
+df_com_mean = mon_sum1.div(3.0)
 
 #============================================== Plotting ===============================================
 
